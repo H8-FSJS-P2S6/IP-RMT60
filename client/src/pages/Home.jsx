c1f72769f5fdfa7073aebae3181cf882d9f0eb9f
hafizhabdul <abdulhafizhsaenal@gmail.com>
2025-04-29 11:25:47 +0700

    chore: update server side api/public

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="alert alert-primary" role="alert">
+            Ini adalah alert Bootstrap
+          </div>
+        </div>
+        <div className="col-md-6">
+          <button className="btn btn-success">Tombol Bootstrap</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,12 @@ const userRoutes = require("./userRoutes");
 const categoryRoutes = require("./categoryRoutes");
 const lectureRoutes = require("./lectureRoutes");
 const cartRoutes = require("./cartRoutes");
+const publicRoutes = require("./publicRoutes");
 
+// Public routes - tidak memerlukan authentication
+router.use("/public", publicRoutes);
+
+// Protected routes - memerlukan authentication
 router.use("/users", userRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/lectures", lectureRoutes);
